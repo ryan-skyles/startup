2926aa26954afc536ef0faa2484f28c13c8d0086
ryan-skyles <ryan.skyles01@gmail.com>
2025-03-14 16:19:40 -0600

    React Part 2 App.jsx changes

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
-
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
 import { Home } from './home/home';
 import { Saved } from './saved/saved';
 import { Account } from './account/account';
+import { AuthState } from './login/authState';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
 
-export default function App() {
-    return <div className="body bg-dark text-light">
-            <BrowserRouter>
+
+function App() {
+    const [userName, setUserName] = React.useState(localStorage.getItem('username') || '');
+    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+    const [authState, setAuthState] = React.useState(currentAuthState);
+
+    return (
+        <BrowserRouter>
+            <div className="body bg-dark text-light">
                 <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
                     <div className="col-md-3 mb-2 mb-md-0">
                         <NavLink className="nav-link px-4" to="home">
@@ -18,32 +24,56 @@ export default function App() {
                         </NavLink>
                     </div>
                     <menu className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                        <li>
+                        {authState == AuthState.Authenticated && ( 
+                            <li>
                             <NavLink className="nav-link px-4" to="home">
                                 Home
                             </NavLink>
-                        </li>
-                        <li>
+                            </li>
+                        )}
+
+                        {authState == AuthState.Authenticated && ( 
+                            <li>
                             <NavLink className="nav-link" to="saved">
                                 Saved
                             </NavLink>
-                        </li>
-                        <li>
-                            <NavLink className="nav-link" to="account">
-                                Account
-                            </NavLink>
-                        </li>
+                            </li>
+                        )}
+
+                        {authState == AuthState.Authenticated && (
+                            <li>
+                                <NavLink className="nav-link" to="account">
+                                    Account
+                                </NavLink>
+                            </li>
+                        )}
                     </menu>
-                    <div className="col-md-3 text-end">
-                        <NavLink className="nav-link" to="">
-                         <button type="button" className="btn btn-outline-primary me-2">Login</button>
-                        </NavLink>
-                    </div>
+                    {authState == AuthState.Authenticated &&(
+                        <div className="col-md-3 text-end">
+                            <NavLink className="nav-link" to="">
+                                <button type="button" className="btn btn-outline-primary me-2">Login</button>
+                            </NavLink>
+                        </div>  
+                    )}
                 </header>
 
                 <Routes>
-                    <Route path='/' element={<Login />} exact />
-                    <Route path='/home' element={<Home />} />
+                    <Route 
+                        path='/'
+                        element={
+                            <Login
+                                userName={userName}
+                                authState={authState}
+                                onAuthChange={(userName, authState) => {
+                                    setAuthState(authState);
+                                    setUserName(userName);
+                                }}
+                            />
+                        }
+                        exact
+                    />
+                    
+                    <Route path='/home' element={<Home userName={userName} />} />
                     <Route path='/saved' element={<Saved />} />
                     <Route path='/account' element={<Account />} />
                     <Route path='*' element={<NotFound />} />
@@ -53,9 +83,15 @@ export default function App() {
                     <span className="text-reset">Author: </span>
                     <a href="https://github.com/ryan-skyles/startup">Ryan Skyles</a>
                 </footer>
-            </BrowserRouter>
-    </div>;
-    function NotFound() {
-        return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
-    }
+            </div>;
+        </BrowserRouter>
+    );
 }
+
+function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+}
+
+export default App;
+
+      
