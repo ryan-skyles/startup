d5fe49de813e2cbb50d2ed2b61aa36f948df833d
ryan-skyles <ryan.skyles01@gmail.com>
2025-03-17 01:34:42 -0600

    React 2 Authenticated.jsx and css creation

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Button from 'react-bootstrap/Button';
-import './login.css';
+import './authenticated.css';
 
 export function Authenticated(props) {
   const navigate = useNavigate();
@@ -13,14 +13,22 @@ export function Authenticated(props) {
   }
 
   return (
-    <div>
-      <div className='playerName'>{props.userName}</div>
-      <Button variant='primary' onClick={() => navigate('/home')}>
-        Home
-      </Button>
-      <Button variant='secondary' onClick={() => logout()}>
-        Logout
-      </Button>
-    </div>
+    <>
+      <main className="form-signin w-100 m-auto">
+        <div class="modal-content rounded-4 shadow">
+          <div className="modal-body p-5 pt-0">
+            <div className="userName text-center" style={{ fontSize: `${Math.max(50, 100 / props.userName.length)}px` }}>
+              Welcome {props.userName}!
+            </div>
+            <Button className="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit" fdprocessedid="8v8edk" variant='warning' onClick={() => navigate('/home')}>
+              Home
+            </Button>
+            <Button className="w-100 mb-2 btn btn-lg rounded-3 btn-primary" variant='secondary' onClick={() => logout()}>
+              Logout
+            </Button>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
