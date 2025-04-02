98510843337e47db597d1aa7db4bf261e31979ee
ryan-skyles <ryan.skyles01@gmail.com>
2025-04-01 20:45:11 -0600

    Service index.js additions

--- service/index.js
+++ service/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 const authCookieName = 'token';
 
-let users = []
+let users = [];
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
@@ -30,4 +30,72 @@ apiRouter.post('/auth/create', async (req, res) => {
     }
   });
 
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
 
+apiRouter.delete('/auth/logout', async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+      delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+});
+
+// const verifyAuth = async (req, res, next) => {
+//     const user = await findUser('token', req.cookies[authCookieName]);
+//     if (user) {
+//         next();
+//     } else {
+//         res.status(401).send({ msg: 'Unauthorized' });
+//     }
+// };
+
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+});
+
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
+
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+}
+  
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
